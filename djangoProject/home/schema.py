15a0f26028404c69e8fae6ef13b2a22b83221e69
Mr.Rezoo <rezam578@gmail.com>
2021-07-08 19:16:05 +0430

    add mutation and filter in query

--- djangoProject/home/schema.py
+++ djangoProject/home/schema.py
@@ -16,6 +16,8 @@ class CarType(DjangoObjectType):
 class HomeQuery(ObjectType):
     persons = graphene.List(PersonType)
     cars = graphene.List(CarType)
+    person = graphene.Field(PersonType, name=graphene.String())
+    car = graphene.Field(CarType, id=graphene.Int())
 
     @staticmethod
     def resolve_persons(parent, info, **kwargs):
@@ -24,3 +26,41 @@ class HomeQuery(ObjectType):
     @staticmethod
     def resolve_cars(parent, info, **kwargs):
         return Car.objects.all()
+
+    @staticmethod
+    def resolve_person(parent, info, **kwargs):
+        person_name = kwargs.get('name')
+        if person_name is not None:
+            return Person.objects.get(name=person_name)
+        return None
+
+    @staticmethod
+    def resolve_car(parent, info, **kwargs):
+        car_id = kwargs.get('id')
+        if car_id is not None:
+            return Car.objects.get(id=car_id)
+        return None
+
+
+class PersonInput(graphene.InputObjectType):
+    name = graphene.String()
+    age = graphene.Int()
+
+
+class CreatePerson(graphene.Mutation):
+    class Arguments:
+        input = PersonInput(required=True)
+
+    # show response
+    person = graphene.Field(PersonType)
+    ok = graphene.Boolean(default_value=False)
+
+    @staticmethod
+    def mutate(parent, info, input=None):
+        person_instance = Person.objects.create(name=input.name, age=input.age)
+        # show response
+        return CreatePerson(person=person_instance, ok=True)
+
+
+class Mutate(graphene.ObjectType):
+    create_person = CreatePerson.Field()
